5001419ae2eb64a7bb8d5660278d23ff745420ba
Keewis <keewis@posteo.de>
2021-01-05 00:26:31 +0100

    outsource the detection into a separate script

---
+++ .github/actions/detect-ci-trigger/script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ref="$0"
+keywords="$1"
+
+git log -n 1 --pretty=format:%s "$ref" | grep -q "$keywords"
+if [[ $? -eq 0]]; then
+    result="true"
+else
+    result="false"
+fi
+
+echo "::set-output name=CI_TRIGGERED::$result"
