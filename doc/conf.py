cbdc735243f241d33f8c7b4df342752a9385889d
Keewis <keewis@posteo.de>
2021-02-07 00:42:43 +0100

    remove the iris intersphinx entry

--- doc/conf.py
+++ doc/conf.py
@@ -12,6 +12,7 @@
 #
 import pathlib
 import sys
+
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
 print("modified path:", sys.path)
 
@@ -21,9 +22,9 @@ print(test)
 
 # -- Project information -----------------------------------------------------
 
-project = 'Test'
-copyright = '2020, Keewis'
-author = 'Keewis'
+project = "Test"
+copyright = "2020, Keewis"
+author = "Keewis"
 
 
 # -- General configuration ---------------------------------------------------
@@ -41,12 +42,12 @@ extensions = [
 master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -56,12 +57,12 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # autosummary
 autosummary_generate = True
@@ -80,7 +81,6 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
     "xarray": ("https://xarray.pydata.org/en/stable", None),
-    "iris": ("https://scitools.org.uk/iris/docs/latest", None),
     "numpy": ("https://numpy.org/doc/stable", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
     "numba": ("https://numba.pydata.org/numba-doc/latest", None),
